7a3e0d93b755665be1b2b021d27bd9013b7ae8b6
03-Bunny-06 <saikowshik0306@gmail.com>
2026-03-21 13:38:49 +0530

    updated-routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,8 @@
 const {Router} = require("express");
-const userRegisterController = require("../controllers/userController");
+const {userRegisterController, userSignInController} = require("../controllers/userController");
 const router = Router();
 
 router.post('/signup', userRegisterController);
+router.post('/signin', userSignInController)
 
 module.exports = router;
