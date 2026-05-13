aee9bc5c3cc23f9a7fc6c08aea9523f6b547885c
03-Bunny-06 <saikowshik0306@gmail.com>
2026-05-13 23:27:13 +0530

    flow-updated

--- routes/userRecruiterRoutes.js
+++ routes/userRecruiterRoutes.js
@@ -14,4 +14,6 @@ router.get('/applications', userMiddleware, recruiterFetchingAllApplications);
 router.get('/applications/:id', userMiddleware, recruiterFetchingSpecificApplication);
 router.patch('/applications/:id/status', userMiddleware, recruiterModifyingtheApplicatonStatus);
 
+
+
 module.exports = router;
