6443af85d202048c77caf1d3ec02e3f67d07d603
03-Bunny-06 <saikowshik0306@gmail.com>
2026-06-11 22:20:35 +0530

    flow-updation

--- middlewares/userAuthMiddleware.js
+++ middlewares/userAuthMiddleware.js
@@ -15,6 +15,7 @@ const userMiddleware = async(req, res, next) => {
     const splitToken = token.split(" ");
     console.log(splitToken);
     const rawToken = splitToken[1];
+    
 
     try{
         const JWT_KEY = process.env.JWT_KEY;
